1a9ed35facc84bcc3e90e462a27cbc0f51e4a38a
kosta <kosta@DESKTOP-4FG5V3I>
2024-01-23 16:44:26 +0900

    변수 테스트

--- 240123/src/VariableTest.java
+++ 240123/src/VariableTest.java
@@ -2,6 +2,7 @@
 public class VariableTest {
 
 	public static void main(String[] args) {
+		
 		boolean isMarried = false;
 		char bloodType = 'A';
 		int age = 30;
@@ -16,6 +17,40 @@ public class VariableTest {
 		System.out.println("결혼여부 : " + isMarried);
 		System.out.println("혈액형 : " + bloodType);
 		System.out.println("이름 : " + name);
+		
+		name = "이이이"; 
+		age = 7;
+		System.out.println(age);
+		System.out.println(name);
+		
+		final double pi = 3.14f;
+//		pi = 3.141f; >> final은 변경이 안됨.
+		
+		char ch = 'g';
+		char ch2 = ' ';
+		String str1 = null;
+		String str2 = "";
+		
+		int value = 10 + 20;
+		System.out.println(value);
+		
+		String str3 = "안녕하세요? ";
+		String str4 = "방가방가";
+		
+		System.out.println(str3+str4);
+		
+		System.out.println("7"+7+7);
+		System.out.println(7+7+"7");
+		
+		System.out.println(name+", "+age+", "+height+", "+weight);
+		
+		float f = 1.6f;
+		double d = 1.6F;
+		double d2 = 3.23;
+		
+		float f2 = (float)d2; // 강제 형변환 : 특정 데이터나 변수 앞에 일시적으로 타입을 작성하여 형변환 시킴
+		
+		
 	}
 
 }
