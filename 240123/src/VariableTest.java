51c08944bd4adf22f64ac7c9919f8c6599907679
kosta <kosta@DESKTOP-4FG5V3I>
2024-01-23 15:11:34 +0900

    za

---
+++ 240123/src/VariableTest.java
@@ -0,0 +1,14 @@
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		int age = 30;
+		double height = 161.2;
+		float weight = 34.3f;
+		
+		System.out.println(age);
+		System.out.println(height);
+		System.out.println(weight);
+	}
+
+}
--- FirstProj/src/HelloWorld.java
+++ FirstProj/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("hello World~");
-		System.out.println("깃허브연동");
+		System.out.println("깃허브연동z");
 	}
 
 }
