26290ffef2b88e29f14a9b97ff7db75a56a1233f
kosta <kosta@DESKTOP-4FG5V3I>
2024-01-23 15:18:33 +0900

    ㅋㅁ

--- 240123/src/VariableTest.java
+++ 240123/src/VariableTest.java
@@ -2,13 +2,20 @@
 public class VariableTest {
 
 	public static void main(String[] args) {
+		boolean isMarried = false;
+		char bloodType = 'A';
 		int age = 30;
 		double height = 161.2;
 		float weight = 34.3f;
-		
-		System.out.println(age);
-		System.out.println(height);
-		System.out.println(weight);
+//		byte b = 128; >> overflow
+		String name = "이현민";
+				
+		System.out.println("나이 : " + age);
+		System.out.println("키 : " + height);
+		System.out.println("몸무게 : " + weight);
+		System.out.println("결혼여부 : " + isMarried);
+		System.out.println("혈액형 : " + bloodType);
+		System.out.println("이름 : " + name);
 	}
 
 }
