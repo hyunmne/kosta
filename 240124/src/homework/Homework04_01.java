d5bb9dd82240ff796ba94ac9ad4bf67f2c81ab27
hyunmne <echo0913@naver.com>
2024-01-24 17:42:58 +0900

    연습문제!

---
+++ 240124/src/homework/Homework04_01.java
@@ -0,0 +1,17 @@
+package homework;
+
+public class Homework04_01 {
+	public static void main(String[] args) {
+		
+//		 다음의 문장들을 조건식으로 표현하라.
+//		 1. int형 변수 x가 10보다 크고 20보다 작을 때 true인 조건식
+//		 2. char형 변수 ch가 공백이나 탭이 아닐 때 true인 조건식
+//		 3. char형 변수 ch가 ‘x' 또는 ’X'일 때 true인 조건식
+//		 4. char형 변수 ch가 숫자(‘0’~‘9’)일 때 true인 조건식
+//		 5. char형 변수 ch가 영문자(대문자 또는 소문자)일 때 true인 조건식
+//		 6. int형 변수 year가 400으로 나눠떨어지거나 또는 4로 나눠떨어지고 100으로 나눠떨어지지
+//		  않을 때 true인 조건식
+//		 7. boolean형 변수 powerOn가 false일 때 true인 조건식
+//		 8. 문자열 참조변수 str이 “yes”일 때 true인 조건식
+	}
+}
