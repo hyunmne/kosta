cd00ce8bc415bf9ea35d0063d7506f03c669fccc
hyunmne <echo0913@naver.com>
2024-01-24 16:25:18 +0900

    for

--- 240124/src/test/ForTest2.java
+++ 240124/src/test/ForTest2.java
@@ -7,12 +7,22 @@ public class ForTest2 {
 //			System.out.println("2 * " + i +" = "+ (i*2));
 //		}
 		
-		for (int k=1; k<=9; k++) {
-			for (int i=2; i<=9; i++) {
-				System.out.print(i + " x " + k +" = "+ (i*k) + "\t");
+//		for (int k=1; k<=9; k++) {
+//			for (int i=2; i<=9; i++) {
+//				System.out.print(i + " x " + k +" = "+ (i*k) + "\t");
+//			}
+//			System.out.println();
+//		}
+		
+		
+		int j = 1;
+
+		while (j < 10) {
+			for (int i=2; i<= 9; i++) {
+				System.out.print(i + " x " + j +" = "+ (i*j) + "\t");
 			}
+			j++;
 			System.out.println();
 		}
-		
 	}
 }
