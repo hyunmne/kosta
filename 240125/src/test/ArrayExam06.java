af9212a653febe937a813686357264af60e5da55
hyunmne <echo0913@naver.com>
2024-01-25 16:33:26 +0900

    배열 예제 - 문자열 위치 구하기

--- 240125/src/test/ArrayExam06.java
+++ 240125/src/test/ArrayExam06.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 //6개의 문자배열을 만들고 {'J', 'O', 'U', 'G', 'O', 'L'}으로 초기화 한 후
 //문자 한 개를 입력 받아 배열에서의 위치를 출력하는 프로그램 작성
 //첫번째 위치는 0번이며 배열에 없는 문자가 입력되면 "none"라는 메시지를 출력하고 끝내는 프로그램을 작성
-
+ 
 public class ArrayExam06 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
