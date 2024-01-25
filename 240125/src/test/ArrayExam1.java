406abf30862b69aaffeab767819829617aaaada8
hyunmne <echo0913@naver.com>
2024-01-25 14:16:42 +0900

    배열 예제 - 문자열 뒤집기

---
+++ 240125/src/test/ArrayExam1.java
@@ -0,0 +1,12 @@
+package test;
+
+public class ArrayExam1 {
+	public static void main(String[] args) {
+		char[] carr = {'A','E','C','X','Y','c','b','z','e'};
+		
+		for (int i=carr.length-1; i>=0; i--) {
+			System.out.print(carr[i]+" ");
+		}
+		
+	}
+}
