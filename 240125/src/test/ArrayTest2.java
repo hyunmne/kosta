50e27aa5a069ebc86f942f860cb9fbdd01e54844
hyunmne <echo0913@naver.com>
2024-01-25 10:58:12 +0900

    2차원 배열

---
+++ 240125/src/test/ArrayTest2.java
@@ -0,0 +1,17 @@
+package test;
+
+public class ArrayTest2 {
+	public static void main(String[] args) {
+		int[][] arr = new int [2][3];
+		arr[0][0] = 100;
+		arr[0][1] = 24;
+		arr[0][2] = 77;
+		arr[1][0] = 50;
+		arr[1][1] = 67;
+		arr[1][2] = 99;
+		
+		System.out.println(arr.length);
+		System.out.println(arr[0].length);
+		System.out.println(arr[1].length);
+	}
+}
