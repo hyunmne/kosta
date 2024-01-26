65f6cf6327c748d6d973610c70f86beba6c6e543
hyunmne <echo0913@naver.com>
2024-01-26 17:30:40 +0900

    대문자 > 소문자 변환하기

---
+++ 240126/src/test/ArrayExam2_14.java
@@ -0,0 +1,9 @@
+package test;
+
+public class ArrayExam2_14 {
+	public static void main(String[] args) {
+		char chA = 'D';
+		char cha = (char)('a'+(chA-'A')); // 대문자 > 소문자 변환
+		System.out.println(cha);
+	}
+}
