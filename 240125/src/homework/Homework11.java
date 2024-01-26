f6c93994027a7a0eb106c42f13aba4621e8f2f14
hyunmne <echo0913@naver.com>
2024-01-26 19:01:26 +0900

    연습문제 - 배열의 가로 세로 총합 구하기

--- 240125/src/homework/Homework11.java
+++ 240125/src/homework/Homework11.java
@@ -11,14 +11,24 @@ public class Homework11 {
 						, {40, 40, 40}
 						, {50, 50, 50}};
 		
+		// 가로와 세로로 1이 더 큰 배열을 생성
 		int[][] result = new int[score.length+1][score[0].length+1];
-		
+
 		for(int i=0; i < score.length; i++) {
 			for(int j=0; j < score[i].length; j++) {
-				/*
-				 (1) 알맞은 코드를 넣어 완성하시오. 
-				*/
-			}
+				result[i][j] += score[i][j];
+				result[i][score[i].length] += score[i][j];
+			} 
+			result[score.length][score[0].length] += result[i][score[0].length]; 
+		}
+
+		for(int i=0; i < score[i].length; i++) {
+			for(int j=0; j < score.length; j++) {
+				result[j][i] = score[j][i];
+				result[result[0].length+1][i] += result[j][i];
+			} 
+//			result[result[i].length][score[i].length] 
+//					+= result[result[i].length][score[i].length]; 
 		}
 		
 		for(int i=0; i < result.length;i++) {
