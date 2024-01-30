81f9f3f5a1723e5c86b37113531580ca5e754aaf
hyunmne <echo0913@naver.com>
2024-01-30 16:10:36 +0900

    생성자

--- 240130/src/test/ClassTest01.java
+++ 240130/src/test/ClassTest01.java
@@ -9,6 +9,13 @@ class Person { // 클래스의 정의는 밖에다가 함
 		return "이름: " +name+", 나이: "+age;
 	}
 	
+	Person() {} // 기본 생성자 : 아무것도 없는 것
+	
+	Person(int a, String n) { // 생성자 
+		age = a;
+		name = n;
+	}
+	
 }
 
 public class ClassTest01 {
@@ -21,8 +28,12 @@ public class ClassTest01 {
 		p2.age = 20;
 		p2.name = "고길동";
 		
+		Person p3 = new Person(25, "홍길동");
+		Person p4 = new Person(30, "고길동");
+		
 		System.out.println(p1.info());
 		System.out.println(p2.info());
+		System.out.println(p3.info());
 		
 		Person[] pers = new Person[3]; // Person 타입의 배열 생성 
 		pers[0] = new Person();
