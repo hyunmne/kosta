2b8d690116d60798f46ba0be99b31592da1a0139
hyunmne <echo0913@naver.com>
2024-01-30 15:45:28 +0900

    RecurcieveCall

---
+++ 240130/src/test/RecurcieveCallTest.java
@@ -0,0 +1,13 @@
+package test;
+
+public class RecurcieveCallTest {
+	static int factorial(int n) {
+		if(n==1) return 1;
+		return n*factorial(n-1);
+	}
+
+	public static void main(String[] args) {
+		int res = factorial(5);
+		System.out.println(res);
+	}
+}
