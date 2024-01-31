50072ac319be898a2a480adc9ab2f2a7e3bf4525
hyunmne <echo0913@naver.com>
2024-01-31 10:33:44 +0900

    객체지향 연습문제

--- 240130/src/homework/Homework06_23.java
+++ 240130/src/homework/Homework06_23.java
@@ -8,26 +8,25 @@ package homework;
 //매개변수 : int[] arr - 최대값을 구할 배열
 
 public class Homework06_23 {
-	
-	 static int max(int[] arr){
-			if(arr == null || arr.length == 0)
-				return -999999;
-			
-			int max = arr[0]; 
-			int min;
-			
-			for(int i = 1; i < arr.length ; i++){
-				if(arr[i] > max)
-					max = arr[i];
-			}
-			
-			return max;
+
+	static int max(int[] arr){
+		if(arr == null || arr.length == 0)
+			return -999999;
+
+		int max = arr[0]; 
+
+		for(int i = 1; i < arr.length ; i++){
+			if(arr[i] > max)
+				max = arr[i];
 		}
-	 
+
+		return max;
+	}
+
 	public static void main(String[] args) 
 	{
 		int[] data = {3,2,9,4,7};
-		
+
 		System.out.println(java.util.Arrays.toString(data));
 		System.out.println("최대값:"+max(data));
 		System.out.println("최대값:"+max(null));
