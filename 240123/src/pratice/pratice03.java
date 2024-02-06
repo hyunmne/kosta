981e5c6358df8e79d87e45e07bfb0d2d492bc39c
hyunmne <echo0913@naver.com>
2024-02-06 16:43:18 +0900

    ,,

--- 240123/src/pratice/pratice03.java
+++
@@ -1,9 +0,0 @@
-package pratice;
-
-public class pratice03 {
-	public static void main(String[] args) {
-		int num = 10;
-		String type = num == 0 ? "0" : (num>0 ? "양수":"음수");
-		System.out.println(type);
-	}
-}
