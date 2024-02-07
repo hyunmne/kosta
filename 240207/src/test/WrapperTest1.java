adf6f4df6af43123161efb4b9bfd5793efc67644
hyunmne <echo0913@naver.com>
2024-02-07 17:34:28 +0900

    Wrapper

---
+++ 240207/src/test/WrapperTest1.java
@@ -0,0 +1,15 @@
+package test;
+
+public class WrapperTest1 {
+	public static void main(String[] args) {
+		Integer n = 100;	// Boxing
+		Object o = n; // Upcasting
+		
+		int m = n; // Unboxing
+		
+		Double d = 3.14; // Boxing
+		double dd = d;// UnBoxing
+		
+		Object obj = 100;
+	}
+}
