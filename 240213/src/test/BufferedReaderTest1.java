5a1eceab4cda5aa5ee935c8ebc619852457fc563
hyunmne <echo0913@naver.com>
2024-02-13 16:12:49 +0900

    bufferedReader/Writer

---
+++ 240213/src/test/BufferedReaderTest1.java
@@ -0,0 +1,29 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedReaderTest1 {
+	public static void main(String[] args) {
+		FileReader fr = null;
+		BufferedReader br = null;
+		try {
+			fr = new FileReader("news.txt");
+			br = new BufferedReader(fr);
+			
+			String str = null;
+			while((str=br.readLine())!=null) { // Line을 전체 다 읽어옴 
+				System.out.println(str);
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br!=null) br.close();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
