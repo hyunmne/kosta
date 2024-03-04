15251c36e806c52760f6fad3d5df8d25cd2a5b2a
hyunmne <echo0913@naver.com>
2024-03-04 14:50:53 +0900

    Login - get, post

---
+++ HelloJSP/src/main/java/LoginProcess.java
@@ -0,0 +1,41 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginProcess
+ */
+@WebServlet("/login")
+public class LoginProcess extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginProcess() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * url에 입력값이 올라갈 때.. 
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		
+		System.out.println(id);
+		System.out.println(password);
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=utf-8");
+		response.getWriter().println("로그인 되었습니다.");
+	}
+
+}
