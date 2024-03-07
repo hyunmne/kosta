70fcafb5f9961386258b2aa30221ba0507456db6
hyunmne <echo0913@naver.com>
2024-03-07 16:43:02 +0900

    mvc - 전체 계좌 조회

--- BankDBProj/src/main/java/controller/AllAccountInfo.java
+++ BankDBProj/src/main/java/controller/AllAccountInfo.java
@@ -1,11 +1,17 @@
 package controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dto.Account;
+import service.AccountService;
+import service.AccountServiceImpl;
 
 /**
  * Servlet implementation class AllAccountInfo
@@ -26,15 +32,16 @@ public class AllAccountInfo extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("allAccountInfo.jsp").forward(request, response);
+		try {
+			AccountService accountService = new AccountServiceImpl();
+			request.setAttribute("accs", accountService.allAccountInfo());
+			request.getRequestDispatcher("allAccountInfo.jsp").forward(request, response);
+		} catch(Exception e) {
+			e.printStackTrace();
+			request.setAttribute("err", e.getMessage());
+			request.getRequestDispatcher("allAccountInfo.jsp").forward(request, response);
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 
 }
