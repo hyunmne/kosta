671b8a86ab733fec073d20af32ef8edd6def0c50
hyunmne <echo0913@naver.com>
2024-03-13 11:37:35 +0900

    게시물 전체 조회

---
+++ MybatisTest/src/main/java/controller/BoardList.java
@@ -0,0 +1,43 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.BoardService;
+import service.BoardServiceImpl;
+
+/**
+ * Servlet implementation class BoardList
+ */
+@WebServlet("/boardList")
+public class BoardList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BoardList() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			BoardService brdService = new BoardServiceImpl();
+			brdService.boardListByPage(request);
+			request.getRequestDispatcher("boardList.jsp").forward(request, response);
+		} catch(Exception e) {
+			e.printStackTrace();
+			request.setAttribute("err", "게시판 글 목록 조회 실패.");
+		}
+	}
+
+}
